d834e614010a810a411c84dfd0ec375c4567da71
Victor Firdevsi <vvifi.mail@gmail.com>
2020-07-06 20:02:16 +0200

    Swap actual and expected sides in assetions.

--- tests.py
+++ tests.py
@@ -10,28 +10,28 @@ class TestSolutions(unittest.TestCase):
         expect=('012' + nl + 
                 '012' + nl + 
                 '012' + nl )
-        self.assertEqual(expect, a1.solve())
+        self.assertEqual(a1.solve(), expect)
 
     def test_solution_a2(self):
         from solutions import a2
         expect=('000' + nl + 
                 '111' + nl + 
                 '222' + nl )
-        self.assertEqual(expect, a2.solve())
+        self.assertEqual(a2.solve(), expect)
 
     def test_solution_a3(self):
         from solutions import a3
         expect=('123' + nl + 
                 '456' + nl + 
                 '789' + nl )
-        self.assertEqual(expect, a3.solve())
+        self.assertEqual(a3.solve(), expect)
 
     def test_solution_b(self):
         from solutions import b
         expect=('01234' + nl + 
                 '01234' + nl + 
                 '01234' + nl )
-        self.assertEqual(expect, b.solve())
+        self.assertEqual(b.solve(), expect)
 
     def test_solution_c(self):
         from solutions import c
@@ -39,35 +39,35 @@ class TestSolutions(unittest.TestCase):
                 '0123' + nl + 
                 '0123' + nl + 
                 '0123' + nl )
-        self.assertEqual(expect, c.solve())
+        self.assertEqual(c.solve(), expect)
 
     def test_solution_d(self):
         from solutions import d
         expect=('987' + nl + 
                 '654' + nl + 
                 '321' + nl )
-        self.assertEqual(expect, d.solve())
+        self.assertEqual(d.solve(), expect)
 
     def test_solution_e(self):
         from solutions import e
         expect=('876' + nl + 
                 '543' + nl + 
                 '210' + nl )
-        self.assertEqual(expect, e.solve())
+        self.assertEqual(e.solve(), expect)
 
     def test_solution_f(self):
         from solutions import f
         expect=('121' + nl + 
                 '212' + nl + 
                 '121' + nl )
-        self.assertEqual(expect, f.solve())
+        self.assertEqual(f.solve(), expect)
 
     def test_solution_g(self):
         from solutions import g
         expect=('111' + nl + 
                 '101' + nl + 
                 '111' + nl )
-        self.assertEqual(expect, g.solve())
+        self.assertEqual(g.solve(), expect)
 
 if __name__ == '__main__':
     unittest.main()
